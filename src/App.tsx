0f5c43ca3b7ce964d996587b01f4a60e2eeca1c0
amardeep-SDE <amardeepdwivedi1494@gmail.com>
2024-12-09 19:28:16 +0530

    frontend setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+     <Button className='bg-orange hover:bg-hoverOrange'>Food App</Button>
+    </>
+  )
+}
+
+export default App
