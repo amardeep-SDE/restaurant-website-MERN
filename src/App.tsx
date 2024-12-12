2dd08bfc1355f06f69771ce642e8cb26cceba582
amardeep-SDE <amardeepdwivedi1494@gmail.com>
2024-12-12 16:22:14 +0530

    feat: profile, navbar, hero, restuarant, checkout and footer ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,42 @@
 import Login from "./auth/Login";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import MainLayout from "./MainLayout";
 import Signup from "./auth/Signup";
 import ForgotPassword from "./auth/ForgotPassword";
 import ResetPassword from "./auth/ResetPassword";
 import VerifyEmail from "./auth/VerifyEmail";
+import HereSection from "./components/HeroSection";
+import MainLayout from "./layout/MainLayout";
+import Profile from "./components/Profile";
+import SearchPage from "./components/SearchPage";
+import RestaurantDetail from "./components/RestaurantDetail";
+import Cart from "./components/Cart";
 
 const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
-    // children: [
-    //   {
-    //     path: "/login"
-    //   }
-    // ]
+    children: [
+      {
+        path: "/",
+        element: <HereSection />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+      },
+      {
+        path: "/search/:text",
+        element: <SearchPage />,
+      },
+      {
+        path: "/restaurant/:id",
+        element: <RestaurantDetail />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+    ],
   },
   {
     path: "/login",
