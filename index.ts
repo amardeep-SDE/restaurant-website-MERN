4b439ade15637b9c0c708d7c92e5748b999934ab
amardeep-SDE <amardeepdwivedi1494@gmail.com>
2024-12-13 21:01:40 +0530

    feat: back-end setup, models, controllers, middlewares, routes, db, and utilities

---
+++ index.ts
@@ -0,0 +1,38 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./db/connectDB";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import userRoute from "./routes/user.route";
+import restaurantRoute from "./routes/restaurant.route";
+import menuRoute from "./routes/menu.route";
+import orderRoute from "./routes/order.route";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// default middleware for any mern project
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(express.json());
+app.use(cookieParser());
+const corsOptions = {
+  origin: "https://localhost:5173",
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+// api
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/restaurant", restaurantRoute);
+app.use("/api/v1/menu", menuRoute);
+app.use("/api/v1/order", orderRoute);
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server listen at port ${PORT}!!`);
+});
